962d2cda8d51422fca35c1e9312594cdcf3a52ae
Deny <zmdddamian@gmail.com>
2024-06-24 12:43:48 +0200

    add DynamoDB basic configuration

---
+++ car-repair-shop-backend/shop/src/main/java/car/repair/shop/config/DynamoDBConfig.java
@@ -0,0 +1,25 @@
+package car.repair.shop.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@EnableDynamoDBRepositories(basePackages = "car.repair.shop")
+public class DynamoDBConfig {
+
+    @Bean
+    @Primary
+    public AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("fakeMyKeyId", "fakeSecretAccessKey")))
+                .build();
+    }
+}
