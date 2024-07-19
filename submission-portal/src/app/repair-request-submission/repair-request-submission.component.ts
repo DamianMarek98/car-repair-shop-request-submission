3b7ba6f5adab21bab589e27af0a3fffc5841c05d
Deny <zmdddamian@gmail.com>
2024-07-19 13:21:45 +0200

    add proper phone number validation

--- submission-portal/src/app/repair-request-submission/repair-request-submission.component.ts
+++ submission-portal/src/app/repair-request-submission/repair-request-submission.component.ts
@@ -46,7 +46,9 @@ export class RepairRequestSubmissionComponent implements OnInit {
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      phoneNumber: ['', [Validators.required]], //todo add validator
+      phoneNumber: ['', [Validators.required, Validators.pattern('^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$'
+            + '|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$'
+            + '|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$')]],
       timeSlots: this.fb.array([]),
       asap: new FormControl(false)
     });
